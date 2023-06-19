28b0896f5bd638c9d452569c577949b695a12c0f
Bambi Lukumwena <bambiesther05@gmail.com>
2023-06-19 08:20:55 -0700

    5-printf.c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,1 +1,11 @@
-#!/bin/bash
+#include <studio.h>
+/**
+ * main - A program that prints a line using printf function
+ * Return: 0 (Success)
+ */
+
+int main(void)
+{
+	printf("with proper grammer, but the outcome is a piece of art,\n")
+		return (0);
+}
